eb5f13533c0803db1e25c156ac5d8db74a2c7c8b
James Steinbach <jdsteinbach@gmail.com>
2017-10-16 12:16:21 -0600

    prevent form double-send

--- js/site.js
+++ js/site.js
@@ -70,35 +70,35 @@ Array.prototype.forEach.call(form_boxes, function(el, i){
   });
 });
 
-(function($) {
-  var $cf = $('#contact-form'),
-      $fm = $('.form-message');
+// (function($) {
+//   var $cf = $('#contact-form'),
+//       $fm = $('.form-message');
 
-  if ( $cf.length > 0 ) {
-    $cf.submit(function(e){
-      e.preventDefault();
+//   if ( $cf.length > 0 ) {
+//     $cf.submit(function(e){
+//       e.preventDefault();
 
-      var form_data = $cf.serialize();
+//       var form_data = $cf.serialize();
 
-      $.ajax({
-        type: 'POST',
-        url: $cf.attr('action'),
-        data: form_data
-      })
-      .done(function(response) {
-        $fm.removeClass('error').addClass('success').html(response);
-        $('#name').val('');
-        $('#email').val('');
-        $('#message').val('');
-      })
-      .fail(function(data) {
-        $fm.removeClass('success').addClass('error');
-        if (data.responseText !== '') {
-          $fm.html(data.responseText);
-        } else {
-          $fm.html('<p>Sorry, an error occured and your message could not be sent.</p>');
-        }
-      });
-    });
-  }
-})(jQuery);
+//       $.ajax({
+//         type: 'POST',
+//         url: $cf.attr('action'),
+//         data: form_data
+//       })
+//       .done(function(response) {
+//         $fm.removeClass('error').addClass('success').html(response);
+//         $('#name').val('');
+//         $('#email').val('');
+//         $('#message').val('');
+//       })
+//       .fail(function(data) {
+//         $fm.removeClass('success').addClass('error');
+//         if (data.responseText !== '') {
+//           $fm.html(data.responseText);
+//         } else {
+//           $fm.html('<p>Sorry, an error occured and your message could not be sent.</p>');
+//         }
+//       });
+//     });
+//   }
+// })(jQuery);
