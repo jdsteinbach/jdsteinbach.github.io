28a195358a2629179b747381c7a956ccce3a1916
jdsteinbach <jdsteinbach@users.noreply.github.com>
2020-06-27 02:29:11 +0000

    deploy: 0bedad15ff1ef51a245313cb537a6c1d6122da69

--- js/site.js
+++ js/site.js
@@ -1,123 +1,68 @@
-'use strict';
+/*
+ * Let the DOM know the browser has JS
+ */
+document.body.classList.add('js')
 
-// Let the DOM know the browser has JS
-document.body.classList.add('js');
 
-// Control form fields
-var formBoxes = document.querySelectorAll('.form__field');
+/*
+ * IntersectionObserver for Header Opacity
+ */
+let io = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    const opacity = 1 - (entry.intersectionRatio / 2).toFixed(2)
+    document.body.style.setProperty('--header-opacity', opacity)
+  })
+}, {
+  threshold: Array.apply(null, { length: 101 }).map((x, idx) => idx / 100)
+})
 
-var activateField = function activateField(el) {
-  return el.parentNode.classList.add('is-active');
-};
+io.observe(document.querySelector('.hero'))
 
-var deactivateField = function deactivateField(el) {
-  if (el.value === '') {
-    el.parentNode.classList.remove('is-active');
-  }
-};
 
-var _iteratorNormalCompletion = true;
-var _didIteratorError = false;
-var _iteratorError = undefined;
-
-try {
-  for (var _iterator = formBoxes[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-    var box = _step.value;
-
-    var formInput = box.querySelector('textarea, input');
-
-    deactivateField(formInput);
-
-    formInput.addEventListener('focus', function (e) {
-      return activateField(e.target);
-    });
-    formInput.addEventListener('change', function (e) {
-      return activateField(e.target);
-    });
-    formInput.addEventListener('blur', function (e) {
-      return deactivateField(e.target);
-    });
-  }
-
-  // Fetch-based Form Submission
-} catch (err) {
-  _didIteratorError = true;
-  _iteratorError = err;
-} finally {
-  try {
-    if (!_iteratorNormalCompletion && _iterator.return) {
-      _iterator.return();
-    }
-  } finally {
-    if (_didIteratorError) {
-      throw _iteratorError;
-    }
-  }
-}
-
-var fetchForm = function fetchForm(_ref) {
-  var id = _ref.id,
-      responseID = _ref.responseID,
-      fieldIDs = _ref.fieldIDs,
-      successMsg = _ref.successMsg,
-      failMsg = _ref.failMsg;
-
-  var contactForm = document.getElementById(id);
+/*
+ * Fetch-based Form Submission
+ */
+const fetchForm = ({id, responseID, fieldIDs, successMsg, failMsg}) => {
+  const contactForm = document.getElementById(id)
 
   if (contactForm !== null && window.fetch !== undefined) {
-    var formMessage = document.getElementById(responseID);
+    let formMessage = document.getElementById(responseID)
 
-    contactForm.addEventListener('submit', function (e) {
-      var data = new FormData(contactForm);
+    contactForm.addEventListener('submit', e => {
+      let data = new FormData(contactForm)
 
-      e.preventDefault();
+      e.preventDefault()
 
       if (data.has('web') > -1 && data.get('web') !== '') {
-        formMessage.innerHTML = failMsg;
-        return false;
+        formMessage.innerHTML = failMsg
+        return false
       }
 
-      fetch(contactForm.getAttribute('action'), {
-        method: contactForm.getAttribute('method'),
-        body: data
-      }).then(function (response) {
-        if (response.status < 400) {
-          var _iteratorNormalCompletion2 = true;
-          var _didIteratorError2 = false;
-          var _iteratorError2 = undefined;
-
-          try {
-            for (var _iterator2 = fieldIDs[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-              var _id = _step2.value;
-
-              document.getElementById(_id).value = '';
-            }
-          } catch (err) {
-            _didIteratorError2 = true;
-            _iteratorError2 = err;
-          } finally {
-            try {
-              if (!_iteratorNormalCompletion2 && _iterator2.return) {
-                _iterator2.return();
-              }
-            } finally {
-              if (_didIteratorError2) {
-                throw _iteratorError2;
-              }
-            }
-          }
-
-          formMessage.innerHTML = '<p class="form__response-message is-success">' + successMsg + '</p>';
-        } else {
-          formMessage.innerHTML = '<p class="form__response-message is-error">' + failMsg + '</p>';
+      fetch(
+        contactForm.getAttribute('action'),
+        {
+          method: contactForm.getAttribute('method'),
+          body: data
         }
-      }).catch(function (err) {
-        console.log(err);
-        formMessage.innerHTML = '<p class="form__response-message is-error">' + failMsg + '</p>';
-      });
-    });
+      )
+        .then(response => {
+          if (response.status < 400) {
+            for (let id of fieldIDs) {
+              document.getElementById(id).value = ''
+            }
+
+            formMessage.innerHTML = `<p class="form__response-message is-success">${successMsg}</p>`
+          } else {
+            formMessage.innerHTML = `<p class="form__response-message is-error">${failMsg}</p>`
+          }
+        })
+        .catch(err => {
+          console.log(err)
+          formMessage.innerHTML = `<p class="form__response-message is-error">${failMsg}</p>`
+        })
+    })
   }
-};
+}
 
 fetchForm({
   id: 'contact-form',
@@ -125,68 +70,174 @@ fetchForm({
   fieldIDs: ['name', 'email', 'message'],
   successMsg: 'Thanks, your message has been sent.',
   failMsg: 'Sorry, an error occurred and your message could not be sent.'
-});
+})
 
 fetchForm({
   id: 'mc-embedded-subscribe-form',
   responseID: 'mc_embed_signup_scroll',
   fieldIDs: [],
   successMsg: 'Thanks, check your inbox to confirm your subscription!',
-  failMsg: 'Sorry, an error occurred and you weren’t subscribed. <a class="button" href="https://jamessteinbach.us7.list-manage.com/subscribe/post?u=e06400c5106eb26339f4a0aea&id=35bef0e04e" target="_blank" rel="noopener noreferrer nofollow">Try subscribing here</a>'
-});
+  failMsg: '<p class="error error--mailchimp">Sorry, an error occurred and you weren’t subscribed.</p><a class="button error-button" href="https://jamessteinbach.us7.list-manage.com/subscribe/post?u=e06400c5106eb26339f4a0aea&id=35bef0e04e" target="_blank" rel="noopener noreferrer nofollow">Subscribe Here</a>'
+})
 
+
+/*
+ * Color Theme Toggle
+ */
 if ('localStorage' in window) {
-  var container = document.getElementById('header-nav');
-  var toggle = document.createElement('button');
-  toggle.setAttribute('type', 'button');
+  const toggles = document.querySelectorAll('input[name="theme"]')
 
-  var saveTheme = function saveTheme(isDark) {
-    var theme = isDark ? 'dark' : '';
-    window.localStorage.setItem('theme', theme);
-  };
+  const initTheme = () => {
+    const savedTheme = window.localStorage.getItem('theme')
+    if (savedTheme) {
+      setTheme(savedTheme)
+      Array.from(toggles).find(t => t.value === savedTheme).checked = true
+    }
+  }
 
-  var setTheme = function setTheme(isDark) {
-    isDark = isDark || getTheme();
-    buttonText(isDark);
-    saveTheme(isDark);
-  };
+  const getActiveTheme = () => {
+    return Array.from(toggles).find(t => t.checked).value
+  }
 
-  var getTheme = function getTheme() {
-    return document.body.classList.contains('theme-dark');
-  };
+  const setTheme = theme => {
+    window.localStorage.setItem('theme', theme)
+    document.body.dataset.theme = theme
+  }
 
-  var buttonText = function buttonText(isDark) {
-    toggle.innerHTML = isDark ? 'Light Theme' : 'Dark Theme';
-  };
+  toggles.forEach(t => t.addEventListener('change', e => {
+    e.preventDefault()
+    setTheme(getActiveTheme())
+  }))
 
-  buttonText(getTheme());
-
-  toggle.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.body.classList.toggle('theme-dark');
-    setTheme();
-  });
-
-  toggle.classList.add('toggle');
-
-  container.appendChild(toggle);
+  window.onload = initTheme
 }
 
-var toc = document.querySelector('.post-toc');
 
-if (toc) {
-  toc.setAttribute('aria-hidden', true);
-  var title = toc.querySelector('.post-toc__title');
+/*
+ * Visibility Toggle
+ */
+const visibilityToggle = (toggle, externalClick, escapeKey, focusLeaves) => {
+  externalClick = externalClick || false
+  escapeKey = escapeKey || false
+  focusLeaves = focusLeaves || false
 
-  if (title) {
-    title.addEventListener('click', function (e) {
-      if (toc.getAttribute('aria-hidden') === 'true') {
-        toc.setAttribute('aria-hidden', false);
+  const widget = toggle
+    ? document.getElementById(toggle.getAttribute('aria-controls'))
+    : false
+
+  if (widget) {
+    const open = () => {
+      toggle.setAttribute('aria-expanded', 'true')
+      widget.removeAttribute('aria-hidden')
+    }
+    const close = () => {
+      toggle.setAttribute('aria-expanded', 'false')
+      widget.setAttribute('aria-hidden', 'true')
+    }
+
+    toggle.addEventListener('click', e => {
+      if (widget.getAttribute('aria-hidden') === 'true') {
+        open()
       } else {
-        toc.setAttribute('aria-hidden', true);
+        close()
       }
-    });
+    })
+
+    // Close when focus leaves widget
+    const focusableEls = widget.querySelectorAll('a, button, input, textarea, select')
+
+    const lastFocusableEl = focusableEls[focusableEls.length - 1]
+
+    const widgetHasFocusedLink = nextLink => {
+      return Array.from(focusableEls).filter(link => {
+        return link === nextLink
+      }).length
+    }
+
+    if (focusLeaves && lastFocusableEl) {
+      lastFocusableEl.addEventListener('blur', e => {
+        if (!widgetHasFocusedLink(e.relatedTarget)) {
+          close()
+        }
+      })
+    }
+
+    // Close on Escape key
+    document.addEventListener('keyup', e => {
+      if (
+        escapeKey &&
+        "keyCode" in e &&
+        e.keyCode === 27 &&
+        widget.getAttribute('aria-hidden') !== 'true'
+      ) {
+        close()
+      }
+    })
+
+    // Close on click outside of widget
+    document.addEventListener('click', e => {
+      if (
+        externalClick &&
+        e.target !== toggle &&
+        !widget.contains(e.target) &&
+        widget.getAttribute('aria-hidden') !== 'true'
+      ) {
+        e.preventDefault()
+        close()
+      }
+    })
   }
 }
 
-console.log(['', '               ///  /////////     /////////', '              ///  ///    ///   ///     ///', '             ///  ///     ///  ///     ///', '            ///  ///     ///  ///', '           ///  ///     ///   ///', '          ///  ///     ///      ///', '         ///  ///     ///         ///', '        ///  ///     ///          ///', '///    ///  ///     ///  ///     ///', '///  ///   ///    ///   ///     ///', ' /////    /////////      ////////', ''].join('\n'));
+visibilityToggle(
+  document.getElementById('toc-toggle'),
+  true,
+  true,
+  true
+)
+
+visibilityToggle(
+  document.getElementById('header-menu-toggle')
+)
+
+
+/*
+ * Header Menu Responsive Override
+ */
+const menu = document.getElementById('header-menu')
+
+if (menu) {
+  const headerMQ = window.matchMedia('(min-width: 50em)')
+
+  const headerToggle = mq => {
+    if (mq.matches) {
+      menu.removeAttribute('aria-hidden')
+    } else {
+      menu.setAttribute('aria-hidden', 'true')
+    }
+  }
+
+  headerToggle(headerMQ)
+
+  headerMQ.addListener(headerToggle)
+}
+
+
+/*
+ * Just for Fun
+ */
+console.log([
+  '',
+  '               ///  /////////     /////////',
+  '              ///  ///    ///   ///     ///',
+  '             ///  ///     ///  ///     ///',
+  '            ///  ///     ///  ///',
+  '           ///  ///     ///   ///',
+  '          ///  ///     ///      ///',
+  '         ///  ///     ///         ///',
+  '        ///  ///     ///          ///',
+  '///    ///  ///     ///  ///     ///',
+  '///  ///   ///    ///   ///     ///',
+  ' /////    /////////      ////////',
+  ''
+].join('\n'))
